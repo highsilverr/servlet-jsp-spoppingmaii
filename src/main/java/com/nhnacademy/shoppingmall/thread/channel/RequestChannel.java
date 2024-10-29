f03ebdb48d733152988d21004902d74d55f5acd2
highsiverr <ye90202@gmail.com>
2024-10-29 16:33:59 +0900

    point 구현

--- src/main/java/com/nhnacademy/shoppingmall/thread/channel/RequestChannel.java
+++ src/main/java/com/nhnacademy/shoppingmall/thread/channel/RequestChannel.java
@@ -18,20 +18,27 @@ public class RequestChannel {
     public synchronized ChannelRequest getRequest() throws InterruptedException {
 
         //todo#14-3 queue가 비어있다면 대기합니다.
-
+        while (queue.isEmpty()) {
+            wait(); // 다른 스레드가 요청을 추가할 때까지 대기
+        }
 
         //todo#14-4 queue에서 request 반환합니다.
+        // 큐에서 요청 반환
+        ChannelRequest request = queue.poll(); // 요청을 큐에서 제거하고 반환
+        notifyAll(); // 대기 중인 스레드를 깨운다
+        return request;
 
-        return null;
     }
 
     public synchronized void addRequest(ChannelRequest request) throws InterruptedException {
+        // 큐가 가득 차 있다면 요청이 소비될 때까지 대기
+        while (queue.size() >= queueMaxSize) {
+            wait(); // 큐가 비어질 때까지 대기
+        }
 
-        //todo#14-5 queue가 가득차있다면 요청이 소비될 때까지 대기합니다.
-
-
-        //todo#14-6 queue에 요청을 추가하고 대기하고 있는 스레드를 깨웁니다
-
+        // 큐에 요청을 추가하고 대기 중인 스레드를 깨운다
+        queue.offer(request); // 요청을 큐에 추가
+        notifyAll(); // 대기 중인 스레드를 깨운다
     }
 
 }
--- src/main/java/com/nhnacademy/shoppingmall/thread/request/impl/PointChannelRequest.java
+++ src/main/java/com/nhnacademy/shoppingmall/thread/request/impl/PointChannelRequest.java
@@ -2,16 +2,37 @@ package com.nhnacademy.shoppingmall.thread.request.impl;
 
 import com.nhnacademy.shoppingmall.common.mvc.transaction.DbConnectionThreadLocal;
 import com.nhnacademy.shoppingmall.thread.request.ChannelRequest;
+import com.nhnacademy.shoppingmall.user.domain.User;
+import jakarta.servlet.ServletContext;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class PointChannelRequest extends ChannelRequest {
 
+    private final ServletContext servletContext;
+
+    public PointChannelRequest(ServletContext servletContext){
+        this.servletContext = servletContext;
+    }
+
     @Override
     public void execute() {
         DbConnectionThreadLocal.initialize();
         //todo#14-5 포인트 적립구현, connection은 point적립이 완료되면 반납합니다.
+
+        try{
+            User user = (User) servletContext.getAttribute("user");
+            int pointsToadd = (int) servletContext.getAttribute("points");
+
+            log.debug("Accruing points for user: {}, Points: {}", user.getUserId(), pointsToadd);
+
+            user.setUserPoint(user.getUserPoint() + pointsToadd);
+            log.info("Points accrued successfully for user: {}", user.getUserId());
+        }  catch (Exception e){
+            log.error("Error during point accrual: ", e);
+        } finally {
+            DbConnectionThreadLocal.reset();
+        }
         log.debug("pointChannel execute");
-        DbConnectionThreadLocal.reset();
     }
 }
